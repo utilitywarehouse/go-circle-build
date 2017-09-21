5d90d69ec947fc227adfc55a5e9053137685bff0
sbramin <mail@sbramin.com>
2017-09-21 18:18:36 +0100

    Added a test to stretch circles legs

--- cmd/main.go
+++ cmd/main.go
@@ -3,5 +3,10 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello Circle")
+	fmt.Println(Message())
+}
+
+// Message returns a string msg
+func Message() string {
+	return "Hello Circle"
 }
